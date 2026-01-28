9e426bba541979034a620cd646465b27311317b9
Damien Bradley <damien.bradley@digital.cabinet-office.gov.uk>
2026-01-28 13:47:30 +0000

    feat: testing commit signing

--- security/src/main/kotlin/uk/gov/onelogin/sharing/security/secureArea/SessionSecurity.kt
+++ security/src/main/kotlin/uk/gov/onelogin/sharing/security/secureArea/SessionSecurity.kt
@@ -3,5 +3,6 @@ package uk.gov.onelogin.sharing.security.secureArea
 import java.security.interfaces.ECPublicKey
 
 fun interface SessionSecurity {
+    // Testing commit signing
     fun generateEcPublicKey(algorithm: String, parameterSpec: String): ECPublicKey?
 }
